2baa00ab2aa3c313c2feccee3aa427f1a5d9e858
Recep Terzi <recepterzi-67@hotmail.com>
2022-08-02 15:21:14 +0300

    next.js

---
+++ components/ProgressBar/ProgressBar.js
@@ -0,0 +1,47 @@
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import React, { useEffect, useState } from 'react'
+import {storage} from '../../firebase/index'
+
+const ProgressBar = ({file,setFile}) => {
+
+    const [progress,setProgress] = useState(0)
+    const [error,setError] = useState(null)
+    const [cancel,setCancel] = useState(false)
+    const [url,setUrl] = useState(null)
+
+    useEffect(() => {
+        const storageRef = ref(storage,file.name)
+        const uploadTask = uploadBytesResumable(storageRef, file)
+        uploadTask.on('state_changed',(snap) => {
+            let yuzdelik = (snap.bytesTransferred / snap.totalBytes ) * 100;
+            if (!cancel){
+                setProgress(yuzdelik)
+            }
+        },(err) => {
+            if(!cancel){
+                setError(err)
+            }
+        },() => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                if(!cancel){
+                    setUrl(downloadURL)
+                }
+            })
+        })
+
+        if(url){
+            if(!cancel){
+                setFile(null)
+            }
+        }
+
+        return () => setError(true)
+
+    },[url,setFile])
+
+  return (
+    <div className="progress-bar" style={{width:progress + '%'}}></div>
+  )
+}
+
+export default ProgressBar
